9ee33edc0e1c4cf18703caad2da6aea407d6dd74
御承扬 <2923616405@qq.com>
2019-09-11 21:42:06 +0800

    Initial commit

---
+++ src/Test/Nothing.java
@@ -0,0 +1,20 @@
+package Test;
+
+// !/usr/bin/env jdk1.8
+// encoding:utf-8
+//@software:IntelliJ IDEA
+//@pack:Test
+//@user:彭友聪
+//@date:2019/09/11
+//@time:下午 9:39
+//@project:Netty
+//@file:Nothing.java
+//Author:御承扬
+//email:2923616405@qq.com
+
+
+public class Nothing {
+    public static void main(String[] args) {
+        System.out.println( "Hello Java" );
+    }
+}
